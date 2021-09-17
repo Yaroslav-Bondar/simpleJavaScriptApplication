25d8f3babd4cb17a96631cf8d464acac2abbec9e
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-09-17 16:15:46 +0300

    render catalog from server

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ function render() {
     productsPage.render()
 }
 let CATALOG = []
-render()
+// render()
 // var doesn`t pop up - ?
 // var CATALOG = []
 // server/catalog.json
 // https://api.npoint.io/54f9c1a9e3e1658820c1
+// fetch('https://api.npoint.io/54f9c1a9e3e1658820c1')
 fetch('server/catalog.json')
     // alert(res.json())
+    // .then(res => console.log(res.json()))
     .then(res => res.json())
     .then(body => CATALOG = body)
-    .catch(err => console.log(err))
+    // .then(body => console.log(body))
+render()
+    // .catch(err => console.log(err))
 console.log(CATALOG)
 
